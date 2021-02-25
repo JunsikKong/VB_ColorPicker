85c5889aefe5086b0e94d879648045c842d8c961
js Kong <workjs1124@gmail.com>
2021-02-25 09:56:58 +0900

    시작

---
+++ ColorPicker/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_scan
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.txt_index = New System.Windows.Forms.TextBox()
+        Me.lbl_index = New System.Windows.Forms.Label()
+        Me.btn_scan = New System.Windows.Forms.Button()
+        Me.btn_Del = New System.Windows.Forms.Button()
+        Me.btn_Add = New System.Windows.Forms.Button()
+        Me.btn_Copy = New System.Windows.Forms.Button()
+        Me.lbl_Code = New System.Windows.Forms.Label()
+        Me.lbl_B = New System.Windows.Forms.Label()
+        Me.lbl_G = New System.Windows.Forms.Label()
+        Me.lbl_R = New System.Windows.Forms.Label()
+        Me.lbl_X = New System.Windows.Forms.Label()
+        Me.lbx_xyrgb = New System.Windows.Forms.ListBox()
+        Me.txt_Code = New System.Windows.Forms.TextBox()
+        Me.txt_B = New System.Windows.Forms.TextBox()
+        Me.txt_G = New System.Windows.Forms.TextBox()
+        Me.txt_R = New System.Windows.Forms.TextBox()
+        Me.txt_Y = New System.Windows.Forms.TextBox()
+        Me.txt_X = New System.Windows.Forms.TextBox()
+        Me.tmr_scan = New System.Windows.Forms.Timer(Me.components)
+        Me.lbl_Y = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(90, 22)
+        Me.ToolStripLabel1.Text = "ToolStripLabel1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 276)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(304, 25)
+        Me.ToolStrip1.TabIndex = 41
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'txt_index
+        '
+        Me.txt_index.Location = New System.Drawing.Point(230, 244)
+        Me.txt_index.Name = "txt_index"
+        Me.txt_index.Size = New System.Drawing.Size(64, 21)
+        Me.txt_index.TabIndex = 39
+        Me.txt_index.TabStop = False
+        '
+        'lbl_index
+        '
+        Me.lbl_index.AutoSize = True
+        Me.lbl_index.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_index.Location = New System.Drawing.Point(186, 244)
+        Me.lbl_index.Name = "lbl_index"
+        Me.lbl_index.Size = New System.Drawing.Size(41, 17)
+        Me.lbl_index.TabIndex = 40
+        Me.lbl_index.Text = "Index"
+        '
+        'btn_scan
+        '
+        Me.btn_scan.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_scan.Font = New System.Drawing.Font("굴림", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.btn_scan.Location = New System.Drawing.Point(16, 178)
+        Me.btn_scan.Name = "btn_scan"
+        Me.btn_scan.Size = New System.Drawing.Size(60, 60)
+        Me.btn_scan.TabIndex = 38
+        Me.btn_scan.Text = "▶"
+        Me.btn_scan.UseVisualStyleBackColor = True
+        '
+        'btn_Del
+        '
+        Me.btn_Del.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn_Del.Location = New System.Drawing.Point(230, 217)
+        Me.btn_Del.Name = "btn_Del"
+        Me.btn_Del.Size = New System.Drawing.Size(64, 21)
+        Me.btn_Del.TabIndex = 37
+        Me.btn_Del.Text = "DEL"
+        Me.btn_Del.UseVisualStyleBackColor = True
+        '
+        'btn_Add
+        '
+        Me.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn_Add.Location = New System.Drawing.Point(90, 217)
+        Me.btn_Add.Name = "btn_Add"
+        Me.btn_Add.Size = New System.Drawing.Size(64, 21)
+        Me.btn_Add.TabIndex = 36
+        Me.btn_Add.Text = "ADD"
+        Me.btn_Add.UseVisualStyleBackColor = True
+        '
+        'btn_Copy
+        '
+        Me.btn_Copy.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn_Copy.Location = New System.Drawing.Point(160, 217)
+        Me.btn_Copy.Name = "btn_Copy"
+        Me.btn_Copy.Size = New System.Drawing.Size(64, 21)
+        Me.btn_Copy.TabIndex = 35
+        Me.btn_Copy.Text = "COPY"
+        Me.btn_Copy.UseVisualStyleBackColor = True
+        '
+        'lbl_Code
+        '
+        Me.lbl_Code.AutoSize = True
+        Me.lbl_Code.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_Code.Location = New System.Drawing.Point(11, 244)
+        Me.lbl_Code.Name = "lbl_Code"
+        Me.lbl_Code.Size = New System.Drawing.Size(105, 17)
+        Me.lbl_Code.TabIndex = 34
+        Me.lbl_Code.Text = "Color HEX Code"
+        '
+        'lbl_B
+        '
+        Me.lbl_B.AutoSize = True
+        Me.lbl_B.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_B.Location = New System.Drawing.Point(14, 138)
+        Me.lbl_B.Name = "lbl_B"
+        Me.lbl_B.Size = New System.Drawing.Size(16, 17)
+        Me.lbl_B.TabIndex = 33
+        Me.lbl_B.Text = "B"
+        '
+        'lbl_G
+        '
+        Me.lbl_G.AutoSize = True
+        Me.lbl_G.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_G.Location = New System.Drawing.Point(14, 111)
+        Me.lbl_G.Name = "lbl_G"
+        Me.lbl_G.Size = New System.Drawing.Size(17, 17)
+        Me.lbl_G.TabIndex = 32
+        Me.lbl_G.Text = "G"
+        '
+        'lbl_R
+        '
+        Me.lbl_R.AutoSize = True
+        Me.lbl_R.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_R.Location = New System.Drawing.Point(14, 84)
+        Me.lbl_R.Name = "lbl_R"
+        Me.lbl_R.Size = New System.Drawing.Size(16, 17)
+        Me.lbl_R.TabIndex = 31
+        Me.lbl_R.Text = "R"
+        '
+        'lbl_X
+        '
+        Me.lbl_X.AutoSize = True
+        Me.lbl_X.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_X.Location = New System.Drawing.Point(14, 15)
+        Me.lbl_X.Name = "lbl_X"
+        Me.lbl_X.Size = New System.Drawing.Size(16, 17)
+        Me.lbl_X.TabIndex = 29
+        Me.lbl_X.Text = "X"
+        '
+        'lbx_xyrgb
+        '
+        Me.lbx_xyrgb.FormattingEnabled = True
+        Me.lbx_xyrgb.ItemHeight = 12
+        Me.lbx_xyrgb.Location = New System.Drawing.Point(90, 15)
+        Me.lbx_xyrgb.Name = "lbx_xyrgb"
+        Me.lbx_xyrgb.ScrollAlwaysVisible = True
+        Me.lbx_xyrgb.Size = New System.Drawing.Size(204, 196)
+        Me.lbx_xyrgb.TabIndex = 28
+        '
+        'txt_Code
+        '
+        Me.txt_Code.Location = New System.Drawing.Point(116, 244)
+        Me.txt_Code.Name = "txt_Code"
+        Me.txt_Code.ReadOnly = True
+        Me.txt_Code.Size = New System.Drawing.Size(64, 21)
+        Me.txt_Code.TabIndex = 27
+        Me.txt_Code.TabStop = False
+        Me.txt_Code.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txt_B
+        '
+        Me.txt_B.Location = New System.Drawing.Point(36, 138)
+        Me.txt_B.Name = "txt_B"
+        Me.txt_B.ReadOnly = True
+        Me.txt_B.Size = New System.Drawing.Size(40, 21)
+        Me.txt_B.TabIndex = 26
+        Me.txt_B.TabStop = False
+        '
+        'txt_G
+        '
+        Me.txt_G.Location = New System.Drawing.Point(36, 111)
+        Me.txt_G.Name = "txt_G"
+        Me.txt_G.ReadOnly = True
+        Me.txt_G.Size = New System.Drawing.Size(40, 21)
+        Me.txt_G.TabIndex = 25
+        Me.txt_G.TabStop = False
+        '
+        'txt_R
+        '
+        Me.txt_R.Location = New System.Drawing.Point(36, 84)
+        Me.txt_R.Name = "txt_R"
+        Me.txt_R.ReadOnly = True
+        Me.txt_R.Size = New System.Drawing.Size(40, 21)
+        Me.txt_R.TabIndex = 24
+        Me.txt_R.TabStop = False
+        '
+        'txt_Y
+        '
+        Me.txt_Y.Location = New System.Drawing.Point(36, 42)
+        Me.txt_Y.Name = "txt_Y"
+        Me.txt_Y.ReadOnly = True
+        Me.txt_Y.Size = New System.Drawing.Size(40, 21)
+        Me.txt_Y.TabIndex = 23
+        Me.txt_Y.TabStop = False
+        '
+        'txt_X
+        '
+        Me.txt_X.Location = New System.Drawing.Point(36, 15)
+        Me.txt_X.Name = "txt_X"
+        Me.txt_X.ReadOnly = True
+        Me.txt_X.Size = New System.Drawing.Size(40, 21)
+        Me.txt_X.TabIndex = 22
+        Me.txt_X.TabStop = False
+        '
+        'tmr_scan
+        '
+        Me.tmr_scan.Interval = 200
+        '
+        'lbl_Y
+        '
+        Me.lbl_Y.AutoSize = True
+        Me.lbl_Y.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.lbl_Y.Location = New System.Drawing.Point(14, 42)
+        Me.lbl_Y.Name = "lbl_Y"
+        Me.lbl_Y.Size = New System.Drawing.Size(15, 17)
+        Me.lbl_Y.TabIndex = 30
+        Me.lbl_Y.Text = "Y"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'frm_scan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 301)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.txt_index)
+        Me.Controls.Add(Me.lbl_index)
+        Me.Controls.Add(Me.btn_scan)
+        Me.Controls.Add(Me.btn_Del)
+        Me.Controls.Add(Me.btn_Add)
+        Me.Controls.Add(Me.btn_Copy)
+        Me.Controls.Add(Me.lbl_Code)
+        Me.Controls.Add(Me.lbl_B)
+        Me.Controls.Add(Me.lbl_G)
+        Me.Controls.Add(Me.lbl_R)
+        Me.Controls.Add(Me.lbl_X)
+        Me.Controls.Add(Me.lbx_xyrgb)
+        Me.Controls.Add(Me.txt_Code)
+        Me.Controls.Add(Me.txt_B)
+        Me.Controls.Add(Me.txt_G)
+        Me.Controls.Add(Me.txt_R)
+        Me.Controls.Add(Me.txt_Y)
+        Me.Controls.Add(Me.txt_X)
+        Me.Controls.Add(Me.lbl_Y)
+        Me.MaximizeBox = False
+        Me.Name = "frm_scan"
+        Me.Text = "XYRGB SCANNER / F9 -ADD"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents txt_index As TextBox
+    Friend WithEvents lbl_index As Label
+    Friend WithEvents btn_scan As Button
+    Friend WithEvents btn_Del As Button
+    Friend WithEvents btn_Add As Button
+    Friend WithEvents btn_Copy As Button
+    Friend WithEvents lbl_Code As Label
+    Friend WithEvents lbl_B As Label
+    Friend WithEvents lbl_G As Label
+    Friend WithEvents lbl_R As Label
+    Friend WithEvents lbl_X As Label
+    Friend WithEvents lbx_xyrgb As ListBox
+    Friend WithEvents txt_Code As TextBox
+    Friend WithEvents txt_B As TextBox
+    Friend WithEvents txt_G As TextBox
+    Friend WithEvents txt_R As TextBox
+    Friend WithEvents txt_Y As TextBox
+    Friend WithEvents txt_X As TextBox
+    Friend WithEvents tmr_scan As Timer
+    Friend WithEvents lbl_Y As Label
+    Friend WithEvents ImageList1 As ImageList
+End Class
---
+++ ColorPicker/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class frm_scan
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Long) As Integer '다른 프로그램에서도 키 입력가능
+    Public Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal duFlage As Long) ' 마우스 동작 이벤트
+    Private Declare Function GetPixel Lib "gdi32" (ByVal hdc As Integer, ByVal x As Integer, ByVal y As Integer) As Integer '픽셀 입력받기
+    Private Declare Function GetWindowDC Lib "user32" (ByVal hwnd As Integer) As Integer '좌표 입력받기
+    Dim scan_on As Boolean = False, memo As String
+    Dim Hex_R As String, Hex_G As String, Hex_B As String
+
+    'RGB 출력 설정
+    Dim bmp As New Bitmap(1, 1)
+
+    Private ReH, ReS, ReV As Double
+
+    Private Sub RGBToHSV(ByVal rr As Single, ByVal gg As Single, ByVal bb As Single)
+        Dim delta As Double, min As Double
+        Dim h As Double = 0, s As Double, v As Double
+
+        min = Math.Min(Math.Min(rr, gg), bb) '55
+        v = Math.Max(Math.Max(rr, gg), bb) '75
+        delta = v - min '20
+
+        If v = 0.0 Then
+            s = 0
+        Else '0.266666..., 0.0574712
+            s = delta / v
+        End If
+
+        If s = 0 Then
+            h = 0.0
+        Else
+            If rr = v Then
+                h = (gg - bb) / delta
+            ElseIf gg = v Then
+                h = 2 + (bb - rr) / delta
+            ElseIf bb = v Then
+                h = 4 + (rr - gg) / delta '-0.8, 4.3
+            End If
+
+            h *= 60 '-48,258
+
+            If h < 0.0 Then
+                h = h + 360 '312
+            End If
+        End If
+
+        ReH = Math.Round(h, 0)
+        ReS = Math.Round(s * 100, 0)
+        ReV = Math.Round(v * 100 / 255, 0)
+
+    End Sub
+
+
+
+    Private Sub tmr_scan_Tick(sender As Object, e As EventArgs) Handles tmr_scan.Tick
+        'XY 출력 설정
+        Dim mouse_pos As Point = MousePosition
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.CopyFromScreen(Windows.Forms.Cursor.Position, New Point(0, 0), New Size(1, 1))
+        End Using
+        Dim pixel As Drawing.Color = bmp.GetPixel(0, 0)
+
+        'btn_scan에 색상 출력
+        btn_scan.BackColor = pixel
+
+        'XYRGB 값 텍스트박스에 출력
+        txt_X.Text = mouse_pos.X
+        txt_Y.Text = mouse_pos.Y
+        txt_R.Text = bmp.GetPixel(0, 0).R
+        txt_G.Text = bmp.GetPixel(0, 0).G
+        txt_B.Text = bmp.GetPixel(0, 0).B
+        RGBToHSV(bmp.GetPixel(0, 0).R, bmp.GetPixel(0, 0).G, bmp.GetPixel(0, 0).B)
+        ToolStripLabel1.Text = "H : " & ReH & " ° / S : " & ReS & " % / V : " & ReV & " %"
+
+        '10진수 RGB값 16진수의 두자리로 출력
+        If bmp.GetPixel(0, 0).R < 16 Then
+            Hex_R = "0" + Convert.ToString(bmp.GetPixel(0, 0).R, 16)
+        Else
+            Hex_R = Convert.ToString(bmp.GetPixel(0, 0).R, 16)
+        End If
+
+        If bmp.GetPixel(0, 0).G < 16 Then
+            Hex_G = "0" + Convert.ToString(bmp.GetPixel(0, 0).G, 16)
+        Else
+            Hex_G = Convert.ToString(bmp.GetPixel(0, 0).G, 16)
+        End If
+
+        If bmp.GetPixel(0, 0).B < 16 Then
+            Hex_B = "0" + Convert.ToString(bmp.GetPixel(0, 0).B, 16)
+        Else
+            Hex_B = Convert.ToString(bmp.GetPixel(0, 0).B, 16)
+        End If
+
+        txt_Code.Text = "#" & Hex_R & Hex_G & Hex_B
+        btn_scan.Text = ""
+
+        If GetAsyncKeyState(Keys.F9) Then 'F9키 누르면 클립보드에 XYRGB값 스캔
+            btn_Add_Click(sender, New System.EventArgs())
+        End If
+
+        '마우스 포인터 수동이동
+        If GetAsyncKeyState(Keys.NumPad8) Then
+            Cursor.Position = New Point(mouse_pos.X, mouse_pos.Y - 1)
+        ElseIf GetAsyncKeyState(Keys.NumPad2) Then
+            Cursor.Position = New Point(mouse_pos.X, mouse_pos.Y + 1)
+        ElseIf GetAsyncKeyState(Keys.NumPad4) Then
+            Cursor.Position = New Point(mouse_pos.X - 1, mouse_pos.Y)
+        ElseIf GetAsyncKeyState(Keys.NumPad6) Then
+            Cursor.Position = New Point(mouse_pos.X + 1, mouse_pos.Y)
+        End If
+
+        Me.Invalidate()
+    End Sub
+
+    Private Sub btn_scan_Click(sender As Object, e As EventArgs) Handles btn_scan.Click
+        If scan_on = True Then
+            btn_scan.BackColor = Color.FromArgb(240, 240, 240)
+            btn_scan.Text = "▶"
+            tmr_scan.Enabled = False
+            scan_on = False
+        ElseIf scan_on = False Then
+            tmr_scan.Enabled = True
+            scan_on = True
+        End If
+    End Sub
+
+
+
+    Private Sub btn_Add_Click(sender As Object, e As EventArgs) Handles btn_Add.Click
+        If txt_X.Text <> "" And txt_Y.Text <> "" And txt_R.Text <> "" And txt_G.Text <> "" And txt_B.Text <> "" Then
+            If txt_index.Text <> "" Then
+                memo = "[" & txt_index.Text & "] "
+            ElseIf txt_index.Text = "" Then
+                memo = ""
+            End If
+            lbx_xyrgb.Items.Add(memo & txt_X.Text & ", " & txt_Y.Text & ", " & txt_R.Text & ", " & txt_G.Text & ", " & txt_B.Text)
+            txt_index.Focus()
+            '리스트 추가할 때마다 마지막 아이템 선택
+            lbx_xyrgb.SelectedIndex = lbx_xyrgb.Items.Count - 1
+        End If
+    End Sub
+
+    Private Sub btn_Del_Click(sender As Object, e As EventArgs) Handles btn_Del.Click
+        lbx_xyrgb.Items.Remove(lbx_xyrgb.SelectedItem)
+        '리스트박스 다중 선택시 다중 선택한 아이템 삭제
+        'Dim del_count As Integer = 0
+        'For del_count = 0 To lbx_xyrgb.SelectedItems.Count
+        'lbx_xyrgb.Items.Remove(lbx_xyrgb.SelectedItem)
+        'Next
+    End Sub
+
+    Private Sub btn_Copy_Click(sender As Object, e As EventArgs) Handles btn_Copy.Click
+        If lbx_xyrgb.Items.Count > 0 Then
+            Clipboard.SetText(lbx_xyrgb.SelectedItem)
+        End If
+    End Sub
+End Class
